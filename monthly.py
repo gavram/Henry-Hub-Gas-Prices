5f67cf15f132ce6ca0d1ce15f476794e9f28eb0c
gavram <gaga68@gmail.com>
2019-10-05 20:36:46 +0200

    Update monthly.py
    Monthly Henry Hub natural gas prices

--- monthly.py
+++ monthly.py
@@ -9,7 +9,7 @@ api_url_monthly = "https://api.eia.gov/series/?api_key=" \
                    + "76c7b40fb506528c68e66cc40d1a2670&series_id=NG.RNGWHHD.M"
 
 # Obtaining Monthly Data for Natural Gas Prices and writing in
-# "CSVs\monthly_from_api.csv", using monthly API series_id
+# "CSVs\monthly.csv", using monthly API series_id
 json_data = requests.get(api_url_monthly).json()
 with open('CSVs/monthly.csv', 'w', newline="") as csvfile:
     writer = csv.writer(csvfile)
@@ -24,7 +24,7 @@ with open('CSVs/monthly.csv', 'w', newline="") as csvfile:
 # Displaying chart on screen
 print("Data is stored in CSVs/monthly.csv")
 key = input("Do you want to print a chart? Press 'y'" \
-            + " to print chart or any other key to quit program: " )
+            + "to print chart or any other key to quit program: " )
 if key == 'y':
     # Printing chart
     x = []
